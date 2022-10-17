f36b906a9b2af3544c411ab00e3db52a47eb73bb
TengaIO <samueltenga@gmail.com>
2022-10-18 01:33:15 +0200

    JSON.stringify

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function info() {
 // start is called when your Battlesnake begins a game
 function start(gameState) {
   console.log("GAME START");
-  console.log(gameState);
+  console.log(JSON.stringify(gameState));
 }
 
 // end is called when your Battlesnake finishes a game
