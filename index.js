49bc8c7d311cb85ae4a73ad965fa4809e1b08ae5
Samuel Tenga <samueltenga@gmail.com>
2022-10-29 14:03:44 +0200

    pyytoon fix

--- index.js
+++ index.js
@@ -92,10 +92,10 @@ function move(gameState) {
   allBodies.push((gameState.board.hazards));
   var snakebodies =allBodies.flat(1);
   heads.forEach( head => {
-    snakebodies.push({x: head[x], y: head[y]+1});
-    snakebodies.push({x: head[x], y: head[y]-1});
-    snakebodies.push({x: head[x]+1, y: head[y]});
-    snakebodies.push({x: head[x]-1, y: head[y]});
+    snakebodies.push({x: head.x, y: head.y+1});
+    snakebodies.push({x: head.x, y: head.y-1});
+    snakebodies.push({x: head.x+1, y: head.y});
+    snakebodies.push({x: head.x-1, y: head.y});
   });
 
   //FLOODFILL 
